b96cd097c807d519b60935c03089d08b8dca8a08
Vladyslav Pustovalov <vladyslav.pustovalov@gmail.com>
2024-06-23 16:49:38 +0300

    init

---
+++ src/main/java/com/thedisciplineprogram/TheDisciplineProgramApplication.java
@@ -0,0 +1,13 @@
+package com.thedisciplineprogram;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheDisciplineProgramApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheDisciplineProgramApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/thedisciplineprogram/TheDisciplineProgramApplicationTests.java
@@ -0,0 +1,13 @@
+package com.thedisciplineprogram;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheDisciplineProgramApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
