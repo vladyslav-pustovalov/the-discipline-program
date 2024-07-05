d6aca5254cb5ad6805969ea1d8176f8a9b88e1af
Vladyslav Pustovalov <vladyslav.pustovalov@gmail.com>
2024-07-05 21:37:45 +0300

    HibernateSessionFactoryUtil adding with env variables

--- src/main/java/com/thedisciplineprogram/utils/HibernateSessionFactoryUtil.java
+++ src/main/java/com/thedisciplineprogram/utils/HibernateSessionFactoryUtil.java
@@ -6,15 +6,39 @@ import com.thedisciplineprogram.models.db_entities.User;
 import lombok.extern.slf4j.Slf4j;
 import org.hibernate.HibernateException;
 import org.hibernate.SessionFactory;
-import org.hibernate.boot.registry.StandardServiceRegistry;
-import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.hibernate.cfg.Configuration;
 
 @Slf4j
 public class HibernateSessionFactoryUtil {
     private static SessionFactory sessionFactory;
+    private static final Configuration configuration;
 
     private HibernateSessionFactoryUtil() {
+    }
+
+     static {
+        var url = String.format(
+                "jdbc:postgresql://%s:%s/%s",
+                System.getenv("TDP_DB_URL"),
+                System.getenv("TDP_DB_PORT"),
+                System.getenv("TDP_DB_SCHEMA")
+        );
+
+        configuration = new Configuration();
+        configuration.setProperty("hibernate.connection.characterEncoding", "UTF-8");
+        configuration.setProperty("hibernate.connection.driver_class", "org.postgresql.Driver");
+
+        configuration.setProperty("hibernate.connection.url", url);
+        configuration.setProperty("hibernate.connection.username", System.getenv("TDP_DB_USER"));
+        configuration.setProperty("hibernate.connection.password", System.getenv("TDP_DB_PASS"));
+
+        configuration.setProperty("hibernate.show_sql", "true");
+        configuration.setProperty("hibernate.format_sql", "true");
+        configuration.setProperty("hibernate.hbm2ddl.auto", "update");
+
+        configuration.addAnnotatedClass(User.class);
+        configuration.addAnnotatedClass(Team.class);
+        configuration.addAnnotatedClass(Program.class);
     }
 
     public static SessionFactory getSessionFactory() {
@@ -26,15 +50,7 @@ public class HibernateSessionFactoryUtil {
 
     private static void createSessionFactory() {
         try {
-            Configuration configuration = new Configuration().configure();
-            configuration
-                    .addAnnotatedClass(User.class)
-                    .addAnnotatedClass(Team.class)
-                    .addAnnotatedClass(Program.class);
-            StandardServiceRegistry builder = new StandardServiceRegistryBuilder()
-                    .applySettings(configuration.getProperties())
-                    .build();
-            sessionFactory = configuration.buildSessionFactory(builder);
+            sessionFactory = configuration.buildSessionFactory();
             log.info("SessionFactory created");
         } catch (HibernateException e) {
             log.error(e.getMessage());
