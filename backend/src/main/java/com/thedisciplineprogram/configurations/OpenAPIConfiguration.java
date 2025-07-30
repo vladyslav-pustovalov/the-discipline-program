2347349a0934168077c9dd1838ec4985a80fffdd
vladpustovalov <vladyslav.pustovalov@gmail.com>
2025-07-30 19:52:11 +0200

    Adding OpenAPIConfiguration

---
+++ backend/src/main/java/com/thedisciplineprogram/configurations/OpenAPIConfiguration.java
@@ -0,0 +1,35 @@
+package com.thedisciplineprogram.configurations;
+
+import io.swagger.v3.oas.annotations.OpenAPIDefinition;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeIn;
+import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
+import io.swagger.v3.oas.annotations.info.Contact;
+import io.swagger.v3.oas.annotations.info.Info;
+import io.swagger.v3.oas.annotations.security.SecurityScheme;
+import io.swagger.v3.oas.annotations.servers.Server;
+
+@OpenAPIDefinition(
+        info = @Info(
+                contact = @Contact(
+                        name = "Vladyslav Pustovalov",
+                        email = "vladyslav.pustovalov@gmail.com",
+                        url = "https://github.com/vladyslav-pustovalov"
+                )
+        ),
+        servers = {
+                @Server(
+                        description = "Prod ENV",
+                        url = "https://thedisciplineprogram.com"
+                )
+        }
+)
+@SecurityScheme(
+        name = "bearerAuth",
+        description = "JWT auth description",
+        scheme = "bearer",
+        type = SecuritySchemeType.HTTP,
+        bearerFormat = "JWT",
+        in = SecuritySchemeIn.HEADER
+)
+public class OpenAPIConfiguration {
+}
