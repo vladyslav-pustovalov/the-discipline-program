9db6f6d5592334daa71426c22bb1355a27453727
vladpustovalov <vladyslav.pustovalov@gmail.com>
2025-08-03 18:28:23 +0200

    remove unnecessary annotation

--- backend/src/test/java/com/thedisciplineprogram/AbstractPostgresContainerTest.java
+++ backend/src/test/java/com/thedisciplineprogram/AbstractPostgresContainerTest.java
@@ -1,13 +1,11 @@
 package com.thedisciplineprogram;
 
-import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
 
-@SpringBootTest
 @Testcontainers
 public abstract class AbstractPostgresContainerTest {
 
