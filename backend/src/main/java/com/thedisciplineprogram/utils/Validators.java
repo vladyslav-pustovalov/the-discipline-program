5a8af61a2884abdd07bdcdaff543c7b1083019c3
vladpustovalov <vladyslav.pustovalov@gmail.com>
2025-08-03 14:04:45 +0200

    For validating password add Validators.isValidPassword

---
+++ backend/src/main/java/com/thedisciplineprogram/utils/Validators.java
@@ -0,0 +1,44 @@
+package com.thedisciplineprogram.utils;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+public class Validators {
+
+    private Validators() {}
+
+    public static boolean isValidPassword(String password) {
+
+        if (password.length() < 6 || password.length() > 32) {
+            log.error("Password length should be between 6 and 32");
+            return false;
+        }
+
+        if (password.matches(".*[\"'`\\\\/<>{}\\[\\]();].*")) {
+            log.error("Password contains disallowed symbols");
+            return false;
+        }
+
+        if (!password.matches(".*[A-Z].*")) {
+            log.error("Password does NOT contain an Uppercase letter");
+            return false;
+        }
+
+        if (!password.matches(".*[a-z].*")) {
+            log.error("Password does NOT contain a Lowercase letter");
+            return false;
+        }
+
+        if (!password.matches(".*\\d.*")) {
+            log.error("Password does NOT contain a digit");
+            return false;
+        }
+
+        if (!password.matches(".*[!@#$%^&*\\-_=+|;:'\",.?/\\\\].*")) {
+            log.error("Password does NOT contain a symbol");
+            return false;
+        }
+
+        return true;
+    }
+}
