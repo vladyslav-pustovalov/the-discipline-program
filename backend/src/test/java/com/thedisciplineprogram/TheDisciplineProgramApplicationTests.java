30687ab83820f16f553eeb5d71f498d63ff468ae
vladpustovalov <vladyslav.pustovalov@gmail.com>
2025-08-03 18:00:48 +0200

    test adding testcontainers for tests

--- backend/src/test/java/com/thedisciplineprogram/TheDisciplineProgramApplicationTests.java
+++ backend/src/test/java/com/thedisciplineprogram/TheDisciplineProgramApplicationTests.java
@@ -2,11 +2,9 @@ package com.thedisciplineprogram;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
 
-@ActiveProfiles("test")
-@SpringBootTest(classes = TheDisciplineProgramApplication.class)
-class TheDisciplineProgramApplicationTests {
+@SpringBootTest
+class TheDisciplineProgramApplicationTests extends AbstractPostgresContainerTest {
 
     @SuppressWarnings("EmptyMethod")
     @Test
