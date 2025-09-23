3a542a8b8f4114431b259c2c87f636cb464c7cff
vladyslav.pustovalov <vladyslav.pustovalov@gmail.com>
2025-09-23 22:09:46 +0200

    add mapstruct processor as a dependency

--- backend/src/main/java/com/thedisciplineprogram/utils/mappers/program/DailyProgramMapper.java
+++ backend/src/main/java/com/thedisciplineprogram/utils/mappers/program/DailyProgramMapper.java
@@ -1,19 +1,21 @@
 package com.thedisciplineprogram.utils.mappers.program;
 
-import com.fasterxml.jackson.core.JsonProcessingException;
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.thedisciplineprogram.models.dtos.program.programdetails.DailyProgramDTO;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.core.JsonProcessingException;
 import org.mapstruct.Mapper;
 import org.mapstruct.Named;
 
 @Mapper(componentModel = "spring")
 public interface DailyProgramMapper {
 
+    ObjectMapper objectMapper = new ObjectMapper();
+
     @Named("stringToDailyProgram")
     default DailyProgramDTO stringToDailyProgram(String json) {
         if (json == null) return null;
         try {
-            return new ObjectMapper().readValue(json, DailyProgramDTO.class);
+            return objectMapper.readValue(json, DailyProgramDTO.class);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to parse JSON", e);
         }
@@ -23,7 +25,7 @@ public interface DailyProgramMapper {
     default String dailyProgramToString(DailyProgramDTO dto) {
         if (dto == null) return null;
         try {
-            return new ObjectMapper().writeValueAsString(dto);
+            return objectMapper.writeValueAsString(dto);
         } catch (JsonProcessingException e) {
             throw new RuntimeException("Failed to write JSON", e);
         }
